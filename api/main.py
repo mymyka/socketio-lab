da3cdd4559d049ec4edc4807a64abdfd21bc022f
Mykyta Kasianenko <mka@empat.tech>
2024-09-11 20:40:41 +0300

    ref

--- api/main.py
+++ api/main.py
@@ -1,80 +1,30 @@
+
+import uvicorn
 import socketio
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-from starlette.responses import FileResponse
-from pathlib import Path
-import dataclasses
-from typing import Literal
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
-# Initialize FastAPI and SocketIO
+
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods
-    allow_headers=["*"],  # Allow all headers
-)
 
-sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
-app.mount('/ws', socketio.ASGIApp(sio))
+sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
 
-# Directory where files will be stored
-UPLOAD_DIR = Path("uploads")
-UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+socket_app = socketio.ASGIApp(sio)
+app.mount("/", socket_app)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-@dataclasses.dataclass
-class WatchedFile:
-    path: str
-    state: Literal["playing", "paused"]
+@sio.on("connect")
+async def connect(sid, env):
+    print("New Client Connected to This id :" + " " + str(sid))
 
 
-watched_file = WatchedFile(path="file.txt", state="paused")
-
-
-@app.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
-    file_location = UPLOAD_DIR / file.filename
-
-    # Save file locally
-    with open(file_location, "wb") as f:
-        f.write(file.file.read())
-
-    watched_file.path = file_location
-
-    # Return a URL for the uploaded file
-    file_url = f"/files/{file.filename}"
-    return JSONResponse(content={"file_url": file_url})
-
-
-@app.get("/files/{filename}")
-async def get_file(filename: str):
-    file_path = UPLOAD_DIR / filename
-    if file_path.exists():
-        return FileResponse(file_path)
-    return JSONResponse(content={"error": "File not found"}, status_code=404)
-
-
-@app.post("/change-file/")
-async def change_file_state(new_state: str):
-    watched_file.state = new_state
-    await sio.emit('file_state_changed', {'path': watched_file.path, 'state': watched_file.state})
-    return JSONResponse(content={"message": "File state updated"})
-
-
-@sio.event
-async def connect(sid, environ):
-    await sio.emit('file_state', {'path': watched_file.path, 'state': watched_file.state}, room=sid)
-
-
-@sio.event
+@sio.on("disconnect")
 async def disconnect(sid):
-    print('Client disconnected')
+    print("Client Disconnected: " + " " + str(sid))
 
 
 if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app)
+    uvicorn.run("Soket_io:app", host="0.0.0.0", port=7777, lifespan="on", reload=True)
